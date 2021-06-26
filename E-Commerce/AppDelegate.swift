3d6986542c5275f45785528c59c536fe717618b1
William Santoso <>
2021-06-26 07:55:04 +0700

    facebook sdk init

--- E-Commerce/AppDelegate.swift
+++ E-Commerce/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import CoreData
 import Firebase
+import FirebaseUI
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         return true
+    }
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as! String?
+      if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+        return true
+      }
+      // other URL handling goes here.
+      return false
     }
 
     // MARK: UISceneSession Lifecycle
