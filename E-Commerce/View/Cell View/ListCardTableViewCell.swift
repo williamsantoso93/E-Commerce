d931244e48e1d53699006c214cc90cd15a30dc23
William Santoso <>
2021-06-26 21:47:09 +0700

    list card

---
+++ E-Commerce/View/Cell View/ListCardTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ListCardTableViewCell.swift
+//  E-Commerce
+//
+//  Created by William Santoso on 26/06/21.
+//
+
+import UIKit
+
+class ListCardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ E-Commerce/View/Controller View/PurchaseHistoryViewController.swift
@@ -0,0 +1,49 @@
+//
+//  PurchaseHistoryViewController.swift
+//  E-Commerce
+//
+//  Created by William Santoso on 26/06/21.
+//
+
+import UIKit
+
+class PurchaseHistoryViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        tableView.register(UINib.init(nibName: "ListCardTableViewCell", bundle: nil), forCellReuseIdentifier: "ListCardTableViewCell")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+extension PurchaseHistoryViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCardTableViewCell", for: indexPath) as! ListCardTableViewCell
+        
+        return cell
+    }
+    
+    
+}
